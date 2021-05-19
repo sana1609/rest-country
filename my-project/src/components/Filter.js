a4242c4a065eb6443e59522d70ce6bc6876abafe
saumya verma <2000vermasaumya18@gmail.com>
2021-05-20 02:39:49 +0530

    eliminated all warnings

--- my-project/src/components/Filter.js
+++ my-project/src/components/Filter.js
@@ -5,11 +5,11 @@ const FilterBox = (value) => {
         <div>
             <select onChange = {value.selectValue} className="shadow-md text-primary bg-primary focus:outline-none p-4 rounded">
                 <option className= 'px-2 py-2' value=''>Filter by Region</option>
-                <option className= 'px-2 py-2' value='Asia' className=" p-4">Asia</option>
+                <option className= 'px-2 py-2' value='Asia'>Asia</option>
                 <option className= 'px-2 py-2' value='America' >America</option>
                 <option className= 'px-2 py-2' value='Europe' >Europe</option>
                 <option className= 'px-2 py-2' value='Africa' >Africa</option>
-                <option className= 'px-2 py-2'  value='Oceania' >Oceania</option>
+                <option className= 'px-2 py-2' value='Oceania' >Oceania</option>
             </select>
         </div>
     )
